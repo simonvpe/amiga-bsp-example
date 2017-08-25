89e33520c4c4ecaec090c93fcdc0d4abda9ae5ec
starlord <starlord@localhost.localdomain>
2017-08-26 00:53:28 +0200

    Line is now animated

--- src/main.cpp
+++ src/main.cpp
@@ -9,14 +9,27 @@ int main() {
   constexpr auto chipset_vhposr   = bsp.chipset[Chipset::Register::VHPOSR];
   constexpr auto chipset_color00  = bsp.chipset[Chipset::Register::COLOR00];
 
+  enum class Direction { Up, Down };
+  
+  uint8_t y = 0x40;
+  Direction dir = Direction::Down;
+  
   while(bit_test<6, ciaa_pra>()) {
-    while(0xAC != read_b<chipset_vhposr>())
-      ; // Wait for raster position
+    while(0xff != read_b<chipset_vhposr>())
+      ; // Wait until vblank (ish)
+
+    y += dir == Direction::Down ? 1 : -1;
+
+    if(y >= 0xf0) dir = Direction::Up;
+    if(y <= 0x50) dir = Direction::Down;
+    
+    while(y != read_b<chipset_vhposr>())
+      ; // Wait for raster position to reach y
 
     write_w<chipset_color00>(0xfff);
 
-    while(0xAC == read_b<chipset_vhposr>())
-      ;
+    while(y == read_b<chipset_vhposr>())
+      ; // Wait for raster position to pass y
 
     write_w<chipset_color00>(0x126);
   }
