251f4ee7e8235857943b9d8e44f8116ae4d6934e
Simon Pettersson <simon.v.pettersson@gmail.com>
2017-08-28 08:24:15 +0200

    Cleaned up code

--- src/main.cpp
+++ src/main.cpp
@@ -2,43 +2,50 @@
 #include "drivers/mouse.hpp"
 #include "drivers/palette.hpp"
 
+#include <tuple>
+#include <algorithm>
+
 using cmap::make_map;
 using cmap::map;
 using cmap::lookup;
 
+enum class Direction { Up, Down };
+constexpr inline auto advance_bar(auto bsp, Direction dir, uint8_t pos) {
+  constexpr auto upper_limit = uint8_t{0xf0};
+  constexpr auto lower_limit = uint8_t{0x50};
+  pos += (dir == Direction::Down ? 1 : -1);
+  dir = pos > upper_limit ? Direction::Up : pos < lower_limit ? Direction::Down : dir;
+  return std::make_tuple(dir, pos);
+}
+
+constexpr inline void wait_raster(auto bsp, auto pos) {
+  while(pos != read_b<bsp.chipset[Chipset::Register::VHPOSR]>())
+    ;
+}
+
+constexpr inline void wait_not_raster(auto bsp, auto pos) {
+  while(pos == read_b<bsp.chipset[Chipset::Register::VHPOSR]>())
+    ;
+}
 
 int main() {
   constexpr auto chipset_vposr    = bsp.chipset[Chipset::Register::VPOSR];  
   constexpr auto chipset_vhposr   = bsp.chipset[Chipset::Register::VHPOSR];
 
-  enum class Direction { Up, Down };
+  
   
   uint8_t y = 0x40;
   Direction dir = Direction::Down;
 
   while(!mouse_clicked(bsp)) {
-    
-    while(bit_test<8,chipset_vposr>() || 0x2c != read_b<chipset_vhposr>())
-      ; // Wait until vblank (ish)
-
+    wait_raster(bsp, 0x2c);
     set_color<Color::C00>(bsp, {0,0,0});
-    
-    y += dir == Direction::Down ? 1 : -1;
-
-    if(y >= 0xf0) dir = Direction::Up;
-    if(y <= 0x50) dir = Direction::Down;
-    
-    while(y != read_b<chipset_vhposr>())
-      ; // Wait for raster position to reach y
-
+    std::tie(dir, y) = advance_bar(bsp, dir, y);
+    wait_raster(bsp, y);
     set_color<Color::C00>(bsp, {0,0,15});
-
-    while(y == read_b<chipset_vhposr>())
-      ; // Wait for raster position to pass y
-
+    wait_not_raster(bsp, y);
     set_color<Color::C00>(bsp, {1,2,6});
   }
-
   return 0;
 }
 	   
