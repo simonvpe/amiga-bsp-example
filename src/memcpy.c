0eaea8d6a0ac36ee1ce8fe8efd44b9e5e29c78a0
Simon Pettersson <simon.v.pettersson@gmail.com>
2017-09-01 07:20:11 +0200

    Update memcpy.c

--- src/memcpy.c
+++ src/memcpy.c
@@ -3,6 +3,6 @@
 void* memcpy(void* dest, const void* src, uint32_t count) {
   uint8_t*       p_dest = (uint8_t*)dest;
   const uint8_t* p_src  = (uint8_t*)src;
-  for(uint32_t i = 0 ; i < count ; ++i) *p_dest = *p_src;
+  for(uint32_t i = 0 ; i < count ; ++i) *p_dest[i] = *p_src[i];
   return dest;
 }
